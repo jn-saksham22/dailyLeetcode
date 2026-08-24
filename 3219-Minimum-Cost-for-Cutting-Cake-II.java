9ec59f0d40195888d73f65a2ec05bd4db7287397
Saksham Jain <jainsaksham0022@gmail.com>
2026-08-24 20:48:45 +0530

    [3219] [Time Beats: 58.33%] [Memory Beats: 97.22%] - LeetPush

---
+++ 3219-Minimum-Cost-for-Cutting-Cake-II.java
@@ -0,0 +1,34 @@
+class Solution {
+    public long minimumCost(int m, int n, int[] horizontalCut, int[] verticalCut) {
+       
+        Arrays.sort(horizontalCut);
+        Arrays.sort(verticalCut);
+
+        int h=m-2,v=n-2;
+        int hp=1,vp=1;
+        long cost = 0;
+        while(h >=0 && v >=0){
+            if(verticalCut[v] <= horizontalCut[h]){
+                cost += (horizontalCut[h] * vp);
+                hp++;
+                h--;
+            }
+            else{
+                cost += (verticalCut[v]*hp);
+                vp++;
+                v--;
+            }
+        }
+        while(h >=0){
+            cost += (horizontalCut[h] * vp);
+                hp++;
+                h--; 
+        }
+        while(v >=0){
+            cost += (verticalCut[v]*hp);
+                vp++;
+                v--; 
+        }
+        return cost;
+    }
+}
