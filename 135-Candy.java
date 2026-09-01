73d4f24f2cf2d7a4e32e271bcf6c39327250e132
Saksham Jain <jainsaksham0022@gmail.com>
2026-09-01 17:47:05 +0530

    [135] [Time Beats: N/A] [Memory Beats: N/A] - LeetPush

--- 135-Candy.java
+++ 135-Candy.java
@@ -2,23 +2,25 @@ class Solution {
     public int candy(int[] ratings) {
         int n = ratings.length;
         int[] l = new int[n];
-        int[] r = new int[n];
-        int ans = 0;
         l[0] = 1 ;
-        r[n-1] = 1;
+  
         for(int i=1;i<n;i++){
             if(ratings[i] > ratings[i-1])
                l[i] = l[i-1]+1;
             else l[i] = 1;
         }
+        int curr = 1, r = 1;
+        int ans = Math.max(1,l[n-1]);
         for(int i=n-2;i>=0;i--){
-            if(ratings[i] > ratings[i+1])
-                r[i] = r[i+1]+1;
-            else r[i] = 1;
+            if(ratings[i] > ratings[i+1]){
+                curr = r+1;
+                r = curr;
+            }
+            else curr = 1;
+                
+            ans += Math.max(curr,l[i]);
         }
-        for(int i=0;i<n;i++){
-            ans += Math.max(l[i],r[i]);
-        }
+        
         return ans;
     }
 }
