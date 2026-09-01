d487fd5a805bf10e437c41de70419b8c743b6a63
Saksham Jain <jainsaksham0022@gmail.com>
2026-09-01 21:37:28 +0530

    [1221] [Time Beats: N/A] [Memory Beats: N/A] - LeetPush

---
+++ 1221-Split-a-String-in-Balanced-Strings.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+        int balance = 0;
+        int x = 0;
+        for(char c : s.toCharArray()){
+            if(c == 'L')
+              balance++;
+            else balance--;
+
+            if(balance == 0) x++;
+        }
+        return x;
+    }
+}
