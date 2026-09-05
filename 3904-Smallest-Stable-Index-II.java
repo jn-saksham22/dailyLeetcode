54d5f77ed3196463d5c7aa96f51261da57444026
Saksham Jain <jainsaksham0022@gmail.com>
2026-09-05 21:16:26 +0530

    [3904] [Time Beats: N/A] [Memory Beats: N/A] - LeetPush

---
+++ 3904-Smallest-Stable-Index-II.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+        int n = nums.length;
+        int min[] = new int[n];
+        int mint = Integer.MAX_VALUE;
+        for(int i=n-1;i>=0;i--){
+            if(nums[i] < mint) mint = nums[i];
+            min[i] = mint;
+        }
+        int max = 0;
+        for(int i=0;i<n;i++){
+            if(nums[i] > max) max = nums[i];
+            if(max - min[i] <= k)
+              return i;
+        }
+        return -1;
+    }
+}
