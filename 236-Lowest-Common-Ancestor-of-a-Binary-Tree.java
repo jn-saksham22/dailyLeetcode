474132bc9a6b051427207e96ff28937caa5488d0
Saksham Jain <jainsaksham0022@gmail.com>
2026-09-20 21:23:42 +0530

    [236] [Time Beats: 51.89%] [Memory Beats: 35.31%] - LeetPush

--- 236-Lowest-Common-Ancestor-of-a-Binary-Tree.java
+++ 236-Lowest-Common-Ancestor-of-a-Binary-Tree.java
@@ -9,34 +9,18 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        ArrayList<TreeNode> p1 = new ArrayList<>();
-        ArrayList<TreeNode> p2 = new ArrayList<>();
-
-        getPath(root, p, p1);
-        getPath(root, q, p2);
-
-        int i = 0;
-        for(;i<p1.size() && i<p2.size();i++){
-            if(p1.get(i) != p2.get(i))
-              break;
+        if(root == null || root == p || root == q){
+            return root;
         }
+        TreeNode left = lowestCommonAncestor(root.left,p,q);
+        TreeNode right = lowestCommonAncestor(root.right,p,q);
 
-        return p1.get(i-1);
-    }
-    private boolean getPath(TreeNode root,TreeNode n,ArrayList<TreeNode> path){
-        if(root == null) return false;
-
-        path.add(root);
-
-        if(root == n){
-            return true;
-        }
-        boolean fndL = getPath(root.left,n,path);
-        boolean fndR = getPath(root.right,n,path);
-
-        if(fndL || fndR) return true;
-
-        path.remove(path.size()-1);
-        return false;
+        if(left == null)
+          return right;
+        
+        if(right == null)
+          return left;
+          
+        return root;
     }
 }
